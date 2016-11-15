d058457226392b9beb58d3b63bf12f818e82075a
VikingPaul <wip321@yahoo.com>
2016-11-14 20:23:58 -0600

    stuff

---
+++ Tweeter/Scripts/app/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("app", [])
+
+app.controller("registerCTRL", function($http) {
+    document.getElementById("userName").addEventListener("focusout", () => {
+        console.log("event")
+        var name = $("#userName").val();
+        $http.get("/api/TwitUsername/" + name).then((response) => {
+            console.log(response);
+        });
+    });
+});
